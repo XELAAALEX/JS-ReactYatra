97419f74b2c36632587d812667b290655da67c21
Alex J <xela02660@gmail.com>
2026-02-15 14:03:28 +0530

    Learned to use useContext hooks which helps us to store the values or functions from parent to child without Props Drilling and how to use it with a Practical Example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, {useState} from 'react';
-import { createPortal } from 'react-dom';
-import Button from './Button'
+import Heading from './Heading';
+import Navbar from './Navbar';
+import {ThemeContext} from './context'
 
 const App = () => {
-    const [showModal, toggleModal] = useState(false);
+    const [theme, setTheme] = useState('dark');
 
-    return <>
-      Hey i am inside root <button onClick={() => toggleModal((prev) => !prev)}>Toggle Modal</button>
-
-      {showModal && createPortal(<div>This is modal content</div>, document.body)}
-      {/* {showModal && <div>This is modal content</div>} */}
-    </>
+    return <ThemeContext.Provider value={[theme, setTheme]}>
+          <Navbar/>
+          <Heading/>
+    </ThemeContext.Provider>
 }
 
 export default App
--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {memo, useContext} from 'react';
+import { ThemeContext } from './context';
+const Button = memo((props) => {
+    const {children, clickAction} = props
+    const [theme, setTheme] = useContext(ThemeContext)
+    const handleClick = () => {
+        clickAction?.()
+        setTheme((prev) => {
+            return prev === 'dark' ? 'light' : 'dark'
+        })
+    }
+    return <button onClick={handleClick}>
+        {children} {theme}
+        </button>
+})
 
-
-export const Button = (props) => {
-let {onClick, text} = props;
-const handleOnClick = () => {
-    onClick?.();
-}
-    
-    return <button onClick={handleOnClick}>{text}</button>;
-};
+export default Button;
